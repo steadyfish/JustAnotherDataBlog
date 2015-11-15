fb0220e4a1587765a8b531d67f0434d6a22bd1fd
steadyfish <dhrumins68@gmail.com>
2015-11-15 18:57:52 -0500

    updated code to download routing numbers data for ACH and wire transfers

--- Code/download_usa_bank_routing_numbers.R
+++ Code/download_usa_bank_routing_numbers.R
@@ -27,41 +27,55 @@ lnk$getElementText()
 lnk$clickElement()
 dnld_url = rem_dr$getCurrentUrl()
 
-#getting the data dictionary
-lnk = rem_dr$findElement(using = "link text", "View the FedACH Directory File Format only")
-lnk$getElementText()
-lnk$clickElement()
-
-
-tab = rem_dr$findElement(using = "css selector", "table#format-table")
-tableHTML <- tab$getElementAttribute("outerHTML")[[1]]
-d_dict = read_html(x = tableHTML) %>%
-  html_table() %>%
-  as.data.frame()
-
-names(d_dict) = tolower(str_replace_all(string = names(d_dict), pattern = "[. ]", replacement = "_"))
-
-d_dict$field_name_fmt = d_dict$field_name %>%
-  str_replace_all("[. ]", "_") %>%
-  tolower()
-
-# getting the routing numbers file
-rem_dr$goBack()
-lnk = rem_dr$findElement(using = "link text", "Receive All FedACH Participant RDFIs with commercial receipt volume")
-rem_dr$executeScript("arguments[0].setAttribute('target', arguments[1]);", list(lnk, ""));
-lnk$getElementText()
-lnk$clickElement()
-
-d_txt = rem_dr$getPageSource()
-d_txt1 = rem_dr$findElement(using = "css selector", "pre")
-d_txt2 = d_txt1$getElementText()[[1]]
-
-d_txt3 = read.fwf(file = textConnection(d_txt2), #http://www.fededirectory.frb.org/FedACHdir.txt
+download_data = function(rem_dr, file_link_text, data_dict_link_text){
+  #getting the data dictionary
+  lnk = rem_dr$findElement(using = "link text", data_dict_link_text)
+  lnk$getElementText()
+  lnk$clickElement()
+  
+  
+  tab = rem_dr$findElement(using = "css selector", "table#format-table")
+  tableHTML <- tab$getElementAttribute("outerHTML")[[1]]
+  d_dict = read_html(x = tableHTML) %>%
+    html_table() %>%
+    as.data.frame()
+  
+  names(d_dict) = tolower(str_replace_all(string = names(d_dict), pattern = "[. ]", replacement = "_"))
+  
+  d_dict$field_name_fmt = d_dict$field_name %>%
+    str_replace_all("[. ]", "_") %>%
+    tolower()
+  
+  # getting the routing numbers file
+  rem_dr$goBack()
+  lnk = rem_dr$findElement(using = "link text", file_link_text)
+  rem_dr$executeScript("arguments[0].setAttribute('target', arguments[1]);", list(lnk, ""));
+  lnk$getElementText()
+  lnk$clickElement()
+  
+  d_txt = rem_dr$getPageSource()
+  d_txt1 = rem_dr$findElement(using = "css selector", "pre")
+  d_txt2 = d_txt1$getElementText()[[1]]
+  
+  d_txt3 = read.fwf(file = textConnection(d_txt2), #http://www.fededirectory.frb.org/FedACHdir.txt
                     widths = d_dict$length,
                     comment.char="",
                     colClasses = "character")
+  
+  names(d_txt3) = d_dict$field_name_fmt
+  rem_dr$goBack()
+  
+  return(list(d_txt = d_txt3,
+              d_dict = d_dict))
+  
+}
 
-names(d_txt3) = d_dict$field_name_fmt
+l_fedACH = download_data(rem_dr, "Receive All FedACH Participant RDFIs with commercial receipt volume", "View the FedACH Directory File Format only")
+l_fedWIRE = download_data(rem_dr, "Receive All Fedwire Participants", "View the Fedwire Directory File Format only")
+
+
+
+
 
 rem_dr$getCurrentUrl()
 rem_dr$screenshot(display = TRUE)
@@ -69,6 +83,8 @@ rem_dr$screenshot(display = TRUE)
 rem_dr$close()
 
 #output
-write.csv(d_txt3, "Data/usa_bank_routing_numbers.csv")
-write.csv(d_dict, "Data/usa_bank_routing_numbers_data_dictionary.csv")
+write.csv(l_fedACH$d_txt, "Data/usa_bank_routing_numbers.csv")
+write.csv(l_fedACH$d_dict, "Data/usa_bank_routing_numbers_data_dictionary.csv")
 
+write.csv(l_fedWIRE$d_txt, "Data/usa_bank_wire_routing_numbers.csv")
+write.csv(l_fedWIRE$d_dict, "Data/usa_bank_wire_routing_numbers_data_dictionary.csv")
